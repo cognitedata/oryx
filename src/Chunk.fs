06f54d8d48fee3017a0c2293a21635225f7353b0
Dag Brattli <dag.brattli@cognite.com>
2019-08-29 08:17:42 +0200

    Make chunking even more compact

--- src/Chunk.fs
+++ src/Chunk.fs
@@ -3,10 +3,9 @@ namespace Oryx
 module Chunk =
 
     let chunk<'a, 'b, 'c> (chunkSize: int) (maxConcurrency: int) (handler: 'a seq -> HttpHandler<'a, 'b, 'b>) (items: 'a seq) : HttpHandler<'a, 'b list, 'c> =
-        let chunks = items |> Seq.chunkBySize chunkSize
-        let batches = chunks |> Seq.chunkBySize maxConcurrency
-
-        batches
+        items
+        |> Seq.chunkBySize chunkSize
+        |> Seq.chunkBySize maxConcurrency
         |> Seq.map(Seq.map handler >> concurrent)
         |> sequential
         // Collect results
