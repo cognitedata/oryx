aec7014edda93a677d844022701bbc0f360093b7
Dag Brattli <dag.brattli@cognite.com>
2019-10-07 15:41:42 +0200

    Fix deploy parameters

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dotnet pack -c release -p:PackageVersion=$0
-dotnet nuget push src/bin/Release/*.nupkg -s $1 -k $2
+dotnet pack -c release -p:PackageVersion=$1
+dotnet nuget push src/bin/Release/*.nupkg -s $2 -k $3
