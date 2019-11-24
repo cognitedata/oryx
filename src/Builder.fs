634ce1590331ae296dde971524ae11b205967f15
Dag Brattli <dag.brattli@cognite.com>
2019-11-24 21:22:30 +0100

    Improve docs (#25)
    - Add more info to the README.
    - Simplify CE bulder implementation.
    - Add explicit return type for protobuf handler.

--- src/Builder.fs
+++ src/Builder.fs
@@ -7,28 +7,33 @@ open FSharp.Control.Tasks.V2.ContextInsensitive
 
 
 type RequestBuilder () =
-    member this.Zero () : HttpHandler<HttpResponseMessage, HttpResponseMessage, _, 'err> = fun next _ -> next Context.defaultContext
-    member this.Return (res: 'a) : HttpHandler<HttpResponseMessage, 'a, _, 'err> = fun next _ ->  next { Request = Context.defaultRequest; Response = res }
+    member this.Zero () : HttpHandler<HttpResponseMessage, HttpResponseMessage, _, 'err> =
+        fun next _ ->
+            next Context.defaultContext
 
-    member this.Return (req: HttpRequest) : HttpHandler<HttpResponseMessage, HttpResponseMessage, _, 'err> = fun next ctx -> next { Request = req; Response = Context.defaultResult }
+    member this.Return (res: 'a) : HttpHandler<HttpResponseMessage, 'a, _, 'err> =
+        fun next _ ->
+            next { Request = Context.defaultRequest; Response = res }
 
-    member this.ReturnFrom (req : HttpHandler<'a, 'b, 'r, 'err>)  : HttpHandler<'a, 'b, 'r, 'err> = req
+    member this.Return (req: HttpRequest) : HttpHandler<HttpResponseMessage, HttpResponseMessage, _, 'err> =
+        fun next _ ->
+            next { Request = req; Response = Context.defaultResult }
+
+    member this.ReturnFrom (req : HttpHandler<'a, 'b, 'r, 'err>) : HttpHandler<'a, 'b, 'r, 'err> = req
 
     member this.Delay (fn) = fn ()
 
-    member x.For(source:'a seq, func: 'a -> HttpHandler<'a, 'b, 'b, 'err>) : HttpHandler<'a, 'b list, 'r, 'err>  =
+    member x.For(source:'a seq, func: 'a -> HttpHandler<'a, 'b, 'b, 'err>) : HttpHandler<'a, 'b list, 'r, 'err> =
         source
         |> Seq.map (fun a -> (func a) )
         |> sequential
 
-    member this.Bind(source: HttpHandler<'a, 'b, 'r, 'err>, fn: 'b -> HttpHandler<'a, 'c, 'r, 'err>) :  HttpHandler<'a, 'c, 'r, 'err> =
-        fun (next : NextFunc<'c, 'r, 'err>) (ctx : Context<'a>) ->
-            let next' (cb : Context<'b>) = task {
-                // Run function
-                return! (fn cb.Response) next ctx
-            }
-            // Run source
-            source next' ctx
+    member this.Bind(source: HttpHandler<'a, 'b, 'r, 'err>, fn: 'b -> HttpHandler<'a, 'c, 'r, 'err>) : HttpHandler<'a, 'c, 'r, 'err> =
+        fun next ctx ->
+            let next' (cb : Context<'b>) =
+                fn cb.Response next ctx // Run function in given context
+
+            source next' ctx // Run source is given context
 
 [<AutoOpen>]
 module Builder =
