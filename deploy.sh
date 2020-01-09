82579935f44d0c062540577a010aec93798aaaea
Dag Brattli <dag.brattli@cognite.com>
2020-01-09 21:42:00 +0100

    Fix deployment paths (#34)

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 dotnet pack -c release -p:PackageVersion=$TRAVIS_TAG
 
 dotnet nuget push src/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/Newtonsoft.Json/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/Protobuf/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/System.Text.Json/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/Thoth.Json.Net/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
+dotnet nuget push extensions/Oryx.NewtonsoftJson/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
+dotnet nuget push extensions/Oryx.Protobuf/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
+dotnet nuget push extensions/Oryx.SystemTextJson/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
+dotnet nuget push extensions/Oryx.ThothJsonNet/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
