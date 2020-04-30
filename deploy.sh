a005f61313fb2c10ccfd1c6388b3895519df9fc1
Dag Brattli <dag.brattli@cognite.com>
2020-04-30 15:21:39 +0200

    Move from Travis to GitHub Actions

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-dotnet pack -c release -p:PackageVersion=$TRAVIS_TAG
-
-dotnet nuget push src/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/Oryx.NewtonsoftJson/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/Oryx.Protobuf/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/Oryx.SystemTextJson/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push extensions/Oryx.ThothJsonNet/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
