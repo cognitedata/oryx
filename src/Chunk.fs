e137a154b5d673fb2e3c418b2bfce2b5a5110336
Dag Brattli <dag.brattli@cognite.com>
2021-02-21 10:55:40 +0100

    Fix chunk

--- src/Chunk.fs
+++ src/Chunk.fs
@@ -3,21 +3,18 @@
 
 namespace Oryx
 
-open System.Net.Http
-
 module Chunk =
-    ()
-// let chunk<'T1, 'TNext, 'TResult, 'TError>
-//     (chunkSize: int)
-//     (maxConcurrency: int)
-//     (handler: seq<'T1> -> HttpHandler<'TSource, seq<'TNext>>)
-//     (items: seq<'T1>)
-//     : HttpHandler<'TSource, seq<'TNext>>
-//     =
-//     items
-//     |> Seq.chunkBySize chunkSize
-//     |> Seq.chunkBySize maxConcurrency
-//     |> Seq.map (Seq.map handler >> concurrent)
-//     |> sequential
-//     // Collect results
-//     >=> map (Seq.ofList >> Seq.collect (Seq.collect id))
+
+    let chunk<'TSource, 'TNext, 'TResult>
+        (chunkSize: int)
+        (maxConcurrency: int)
+        (handler: seq<'TSource> -> HttpHandler<'TSource, seq<'TResult>>)
+        (items: seq<'TSource>)
+        : HttpHandler<'TSource, seq<'TResult>> =
+        items
+        |> Seq.chunkBySize chunkSize
+        |> Seq.chunkBySize maxConcurrency
+        |> Seq.map (Seq.map handler >> concurrent)
+        |> sequential
+        // Collect results
+        >=> map (Seq.ofList >> Seq.collect (Seq.collect id))
