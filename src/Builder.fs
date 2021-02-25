8036279c801c2c2a6e263c375803008d52a2a906
Dag Brattli <dag@brattli.net>
2021-02-25 09:28:24 +0100

    Fix builder bind

--- src/Builder.fs
+++ src/Builder.fs
@@ -29,30 +29,31 @@ type RequestBuilder () =
         handlers |> sequential
 
     /// Binds value of 'TValue for let! All handlers runs in same context within the builder.
+    /// Binds value of 'TValue for let! All handlers runs in same context within the builder.
     member _.Bind
         (
-            source: HttpHandler<'TSource, 'TNext>,
-            fn: 'TNext -> HttpHandler<'TNext, 'TResult>
+            source: HttpHandler<'TSource, 'TValue>,
+            fn: 'TValue -> HttpHandler<'TSource, 'TResult>
         ): HttpHandler<'TSource, 'TResult> =
 
         let subscribe (next: IHttpNext<'TResult>) =
-            let (next: IHttpNext<'TNext>) =
-                { new IHttpNext<'TNext> with
+            let (next: IHttpNext<'TValue>) =
+                { new IHttpNext<'TValue> with
                     member _.NextAsync(ctx, ?content) =
                         task {
                             let obv =
                                 { new IHttpNext<'TResult> with
-                                    member _.NextAsync(ctx', content) = next.NextAsync(ctx, ?content = content)
-                                    member _.ErrorAsync(ctx, exn) = next.ErrorAsync(ctx, exn)
+                                    member _.NextAsync(ctx', content) =
+                                        next.NextAsync(Context.merge [ ctx; ctx' ], ?content = content)
+
+                                    member _.ErrorAsync(ctx', exn) = next.ErrorAsync(Context.merge [ ctx; ctx' ], exn)
                                 }
 
                             match content with
                             | Some content ->
-                                let res = fn content
+                                let res: HttpHandler<'TSource, 'TResult> = fn content
 
-                                return!
-                                    res.Subscribe(obv)
-                                    |> (fun obv -> obv.NextAsync(ctx, content = content))
+                                return! res.Subscribe(obv).NextAsync(ctx)
                             | None -> return! obv.NextAsync(ctx)
                         }
 
