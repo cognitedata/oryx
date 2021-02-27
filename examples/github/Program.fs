10998dc6bf0bc935a93a1b52dde9f374717bae17
Dag Brattli <dag@brattli.net>
2021-02-27 12:58:26 +0100

    Add github example

---
+++ examples/github/Program.fs
@@ -0,0 +1,32 @@
+open Oryx
+open System.Net.Http
+open Oryx.ThothJsonNet.ResponseReader
+open Thoth.Json.Net
+open FSharp.Control.Tasks.ContextInsensitive
+
+[<EntryPoint>]
+let main argv =
+    use client = new HttpClient()
+
+    let context =
+        Context.defaultContext
+        |> Context.withHttpClient client
+
+    let response =
+        GET
+        >=> withUrl "https://api.github.com/repos/cognitedata/oryx/releases/latest"
+        >=> fetch
+        >=> json (Decode.field "tag_name" Decode.string)
+
+    let _ =
+        (task {
+            let! tag = (response |> runAsync context)
+
+            printfn "%A" tag
+
+            return 0
+         })
+            .GetAwaiter()
+            .GetResult()
+
+    0 // return an integer exit code
