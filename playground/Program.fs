0c0bb15a61e527041b300536733cbaca8305cd62
Dag Brattli <dag@brattli.net>
2021-02-27 14:17:28 +0100

    Delete unused file

--- playground/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿type FilterFn (fn: seq<obj> -> seq<obj>) =
-    member x.__call__<'T>(source: seq<'T>) : seq<'T> =
-        fn (source |> Seq.map (fun x -> x :> obj))
-        |> Seq.map (fun x -> x :?> 'T)
-
-let take (count: int) : FilterFn =
-    let _take (source: seq<'TSource>) : seq<'TSource> = source
-    FilterFn(_take)
