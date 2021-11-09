f961a5ded88d88ddebff6cbb78743e96a4166589
Ivan Polomani <ivan.polomanyi@cognite.com>
2021-11-09 09:31:53 +0100

    Add labels for metrics (#92)

--- src/HttpHandler/HttpContext.fs
+++ src/HttpHandler/HttpContext.fs
@@ -4,6 +4,7 @@
 namespace Oryx
 
 open System
+open System.Collections
 open System.Diagnostics
 open System.Net
 open System.Net.Http
@@ -83,6 +84,8 @@ and HttpRequest =
       LogFormat: string
       /// Optional Metrics for recording metrics.
       Metrics: IMetrics
+      /// Optional Labels to label the request
+      Labels: Generic.IDictionary<string, string>
       /// Extra state used to e.g build the URL. Clients are free to utilize this property for adding extra
       /// information to the context.
       Items: Map<string, Value>
@@ -129,6 +132,7 @@ module HttpContext =
           LogLevel = LogLevel.None
           LogFormat = defaultLogFormat
           Metrics = EmptyMetrics()
+          Labels = dict []
           Items = Map.empty
           CompletionMode = HttpCompletionOption.ResponseContentRead }
 
