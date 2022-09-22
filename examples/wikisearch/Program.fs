241e3b3d452293d7b01753eaa87de6c937fdfa48
Dag Brattli <dag.brattli@cognite.com>
2022-09-22 11:31:48 +0200

    Fantomas

--- examples/wikisearch/Program.fs
+++ examples/wikisearch/Program.fs
@@ -14,12 +14,12 @@ type WikiSearchHit =
 type WikiSearchHits = WikiSearchHits of WikiSearchHit list
 
 let wikiDataItemDecoder: Decoder<WikiSearchHit> =
-    Decode.oneOf [ Decode.string |> Decode.map SearchTerm
-                   Decode.list Decode.string |> Decode.map SearchHits ]
+    Decode.oneOf
+        [ Decode.string |> Decode.map SearchTerm
+          Decode.list Decode.string |> Decode.map SearchHits ]
 
 let wikiDataItemsDecoders: Decoder<WikiSearchHits> =
-    Decode.list wikiDataItemDecoder
-    |> Decode.map WikiSearchHits
+    Decode.list wikiDataItemDecoder |> Decode.map WikiSearchHits
 
 [<Literal>]
 let Url = "https://en.wikipedia.org/w/api.php"
@@ -27,25 +27,17 @@ let Url = "https://en.wikipedia.org/w/api.php"
 let options = JsonSerializerOptions()
 
 let query term =
-    [ struct ("action", "opensearch")
-      struct ("search", term) ]
+    [ struct ("action", "opensearch"); struct ("search", term) ]
 
 let request ctx term =
-    ctx
-    |> withQuery (query term)
-    |> fetch
-    |> json wikiDataItemsDecoders
+    ctx |> withQuery (query term) |> fetch |> json wikiDataItemsDecoders
 
 let asyncMain _ =
     task {
         use client = new HttpClient()
 
         let ctx: HttpHandler<unit> =
-            httpRequest
-            |> GET
-            |> withHttpClient client
-            |> withUrl Url
-            |> cache
+            httpRequest |> GET |> withHttpClient client |> withUrl Url |> cache
 
         let! result = request ctx "F#" |> runUnsafeAsync
         printfn $"Result: {result}"
