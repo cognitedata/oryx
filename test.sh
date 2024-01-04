a607d585b7c6f54b3b689845810b2ba7c8ae38fc
Ozan Goktan <ozan.goktan@cognite.com>
2024-01-04 09:40:27 +0100

    pass solution directory as a parameter in the test script

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dotnet test test /p:Include="[Oryx]*" /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov'
+dotnet test test /p:Include="[Oryx]*" /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput='../coverage.lcov' /p:SolutionDir=$(pwd)
